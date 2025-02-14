b047d159fd96a22856896ff33045bfc0d6d4d6fb
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2025-02-14 16:08:47 +0600

    Add files via upload

---
+++ 1FreeDrinks.js
@@ -0,0 +1,19 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 600; // You can change this value to test different scenarios
+
+if (burgerPrice > 500) {
+    console.log("Free Coke");
+} else {
+    console.log("Coke: 30tk");
+}
+if (burgerPrice > 500) {
+    console.log("Free Coke");
+} else {
+    console.log("Coke: 30tk");
+}
